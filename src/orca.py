2b67b1c9f43076a9657a32926d61241a9c4c4c49
Luke Brady <lbrad23105@gmail.com>
2016-07-19 23:16:57 -0400

    Update

--- src/orca.py
+++ src/orca.py
@@ -12,12 +12,12 @@ def pullContainer(images = None, tag = "latest"):
         print("Error: No image specified.")
                    
 def instantiateDockerContainer(image = None, tag = "latest", count = 1, name = None, host_port = None, container_port = None, env_variables = None, daemonized = True):
-    if image != None and daemonized == True:
+    if image != None:
         try:
             for x in range(1,count + 1):
                 subprocess.call(["docker","run","-d", "--name", name + "" + str(x),
                                  "-p", str(host_port) + ":" + str(container_port),
-                                 image + ":" + tag], shell = True)
+                                 image + ":" + tag])
                 host_port = host_port + 1
                 
         except IOError:
@@ -32,39 +32,50 @@ def instantiateDockerContainer(image = None, tag = "latest", count = 1, name = N
 
 def main(argv):
     try:
-        opts, args = getopt.getopt(argv,"i:t:h:c:n:d:hp:cp",["image=","tag=","count=","name=","host-port=","container-port=","daemonized="])
+        opts, args = getopt.getopt(argv,"i:t:h:c:n:d:hp:cp",["help=","image=","tag=","count=","name=","host-port=","container-port=","daemonized="])
     except getopt.GetoptError:
         raise getopt.GetoptError
         sys.exit(2)
         
     for opt, arg in opts:
-        if opt == "-h":
-            print("Yo")
+        if opt == ("-h","--help"):
+            print("""Orca Docker Orcastration.
+                     Luke Brady 2016
+            
+                     -h, --help : Built in help.
+                     -i, --image : Specify image you wish to instantiate.
+                     -t, --tag : Tag of the image.
+                     -c, --count : Amount of containers to instantiate.
+                     -n, --name : Names containers.
+                     -hp, --host-port : Port container will be accessible through.
+                     -cp, --container-port : Port that will be accessible to the host.
+                     -d, --daemonized : Boolean that tells docker to daemonize container.
+                     
+                     Hope that helped!
+                  """)
             sys.exit()
         elif opt in ("-i","--image"):
             imagep = str(arg)
         elif opt in ("-t","--tag"):
             tagp = str(arg)
         elif opt in ("-c","--count"):
-            countp = arg
+            countp = int(arg)
         elif opt in ("-n","--name"):
             namep = str(arg)
         elif opt in ("-hp","--host-port"):
-            host_portp = str(arg)
-        elif opt in ("cp","--container-port"):
-            container_portp = str(arg)
+            host_portp = int(arg)
+        elif opt in ("-cp","--container-port"):
+            container_portp = int(arg)
         elif opt in ("-d","--daemonized"):
-            daemonizedp = True
-        else:
-            print("Did not input a valid argument.")
-            sys.exit()
-        
-    instantiateDockerContainer(image = imagep, tag = tagp, count = countp, name = namep,
+            daemonizedp = bool(arg)
+    try:    
+        instantiateDockerContainer(image = imagep, tag = tagp, count = countp, name = namep,
                                host_port = host_portp, container_port = container_portp,
                                daemonized = daemonizedp)
+    except IOError:
+        raise IOError
+    sys.exit()
         
-    
-    
 
 if __name__ == "__main__":
     main(sys.argv[1:])
